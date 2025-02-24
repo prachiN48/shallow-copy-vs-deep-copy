ee7730c53c2f097b7ab0cb38c40d1e1aa59558f6
Prachi Nawathe <prachi.nawathe@thoughtworks.com>
2025-02-24 10:12:48 +0100

    extend books to include publish date

--- components/Store.ts
+++ components/Store.ts
@@ -9,7 +9,8 @@ export interface Store {
 interface Book {
     id: number,
     title: string,
-    author: string
+    author: string,
+    firstPublished?: Date
 }
 
 export const destructuredCopyStore = (store: Store) : Store => {
@@ -20,6 +21,10 @@ export const assignedCopyStore = (store: Store) : Store => {
     return Object.assign({}, store)
 }
 
+export const jsonParsedCloneStore = (store: Store) : Store => {
+    return JSON.parse(JSON.stringify(store))
+}
+
 export const deepCloneStore = (store: Store) : Store => {
     return _.cloneDeep(store)
 }
@@ -28,6 +33,3 @@ export const structuredCloneStore = (store: Store) : Store => {
     return structuredClone(store)
 }
 
-export const jsonParsedCloneStore = (store: Store) : Store => {
-    return JSON.parse(JSON.stringify(store))
-}
--- components/__tests__/Store.test.ts
+++ components/__tests__/Store.test.ts
@@ -6,6 +6,8 @@ import {
     structuredCloneStore
 } from "../Store";
 
+const date = new Date()
+
 describe('destructuredCopy', () => {
     it('should create a copy of the store object', () => {
         const originalStore: Store = {
@@ -56,6 +58,34 @@ describe('assignedCopy', () => {
     })
 })
 
+describe('jsonParsedClone', () => {
+    it('should create a copy of the store object', () => {
+
+        const originalStore: Store = {
+            id: 1,
+            name: 'old book store',
+            books: [
+                {
+                    id: 1,
+                    title: 'book number one',
+                    author: 'author 1',
+                    firstPublished: date
+                }
+            ]
+        }
+        const copyStore = jsonParsedCloneStore(originalStore)
+        copyStore.name = 'new book store'
+        expect(copyStore.name).not.toBe(originalStore.name)
+        // expect(copyStore.books[0].firstPublished).toBe(date)
+        copyStore.books.push({
+            id: 2,
+            title: 'book number 2',
+            author: 'author 2'
+        })
+        expect(copyStore.books.length).not.toBe(originalStore.books.length)
+    })
+})
+
 describe('deepClone', () => {
     it('should create a copy of the store object', () => {
         const originalStore: Store = {
@@ -95,31 +125,6 @@ describe('structuredClone', () => {
             ]
         }
         const copyStore = structuredCloneStore(originalStore)
-        copyStore.name = 'new book store'
-        expect(copyStore.name).not.toBe(originalStore.name)
-        copyStore.books.push({
-            id: 2,
-            title: 'book number 2',
-            author: 'author 2'
-        })
-        expect(copyStore.books.length).not.toBe(originalStore.books.length)
-    })
-})
-
-describe('jsonParsedClone', () => {
-    it('should create a copy of the store object', () => {
-        const originalStore: Store = {
-            id: 1,
-            name: 'old book store',
-            books: [
-                {
-                    id: 1,
-                    title: 'book number one',
-                    author: 'author 1'
-                }
-            ]
-        }
-        const copyStore = jsonParsedCloneStore(originalStore)
         copyStore.name = 'new book store'
         expect(copyStore.name).not.toBe(originalStore.name)
         copyStore.books.push({
